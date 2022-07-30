619816572279ff941aae5a9545e8ab23bb5d6a45
fname lname <monzurulislam997@gmail.com>
2022-07-31 01:54:52 +0600

    mongo client add

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ require('dotenv').config()
 const jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5500
+
+// cors && middleware
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+
+
+
+// mongo client
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zo2yn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
